0bb4689d183e893763b77d82b207ad461a29aabc
George Fouche <fouliex@gmail.com>
2019-10-28 14:27:39 -0700

    cleanup back end

--- ToDoBackend/src/main/java/com/todo/rest/webservices/restfulwebservices/jwt/JwtInMemoryUserDetailsService.java
+++ ToDoBackend/src/main/java/com/todo/rest/webservices/restfulwebservices/jwt/JwtInMemoryUserDetailsService.java
@@ -15,8 +15,6 @@ public class JwtInMemoryUserDetailsService implements UserDetailsService {
     static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
 
     static {
-        inMemoryUserList.add(new JwtUserDetails(1L, "in28minutes",
-                "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2"));
         inMemoryUserList.add(new JwtUserDetails(2L, "georgeerol",
                 "$2a$10$OTJGlpcHdRSwsbzfXmnvV.8Xur7CLBtdV4dA3e4dafv5QP2aSviXS", "ROLE_USER_2"));
     }

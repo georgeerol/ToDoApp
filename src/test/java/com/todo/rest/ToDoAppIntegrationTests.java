00da077ff37eac7c0f96d45589692c34bdd1c99c
George Fouche <fouliex@gmail.com>
2019-11-15 10:58:48 -0800

    clean up

--- src/test/java/com/todo/rest/ToDoAppIntegrationTests.java
+++ src/test/java/com/todo/rest/ToDoAppIntegrationTests.java
@@ -14,15 +14,12 @@ import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {ToDoApp.class}, webEnvironment
-        = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ToDoAppIT {
-
-    private static final String API_ROOT = "http://localhost:8087/users/georgeerol/todos";
-    private static final String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJnZW9yZ2Vlcm9sIiwiZXhwIjoxNTc0Mzk0NzQwLCJpYXQiOjE1NzM3ODk5NDB9.1ShRJ6gscWPNQWRDjt2WzhI92xBmrnpu_sNDowQVbOTytYoF0QWNmDWoQh7FsD7QGbYYji7k8w2hewDgLLG-Bw";
-
+@SpringBootTest(classes = {ToDoApp.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ToDoAppIntegrationTests {
+    
     @LocalServerPort
     private int port;
+    private static final String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJnZW9yZ2Vlcm9sIiwiZXhwIjoxNTc0Mzk0NzQwLCJpYXQiOjE1NzM3ODk5NDB9.1ShRJ6gscWPNQWRDjt2WzhI92xBmrnpu_sNDowQVbOTytYoF0QWNmDWoQh7FsD7QGbYYji7k8w2hewDgLLG-Bw";
 
     @Test
     public void whenGetAllToDos_thenOK() {
@@ -30,9 +27,6 @@ class ToDoAppIT {
         Response response = given().port(port).auth().oauth2(token).get(api);
         assertEquals(HttpStatus.OK.value(), response.getStatusCode());
     }
-
-
-
 
 
 }

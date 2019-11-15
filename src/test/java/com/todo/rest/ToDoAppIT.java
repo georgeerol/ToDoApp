fa04ebf1e01c2e541c5835baac5f8bb0a4f4d6a8
George Fouche <fouliex@gmail.com>
2019-11-14 23:39:49 -0800

    Add travis file

--- src/test/java/com/todo/rest/ToDoAppIT.java
+++ src/test/java/com/todo/rest/ToDoAppIT.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {ToDoApp.class}, webEnvironment
         = SpringBootTest.WebEnvironment.DEFINED_PORT)
-class ToDoAppTests {
+class ToDoAppIT {
 
     private static final String API_ROOT = "http://localhost:8087/users/georgeerol/todos";
     private static final String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJnZW9yZ2Vlcm9sIiwiZXhwIjoxNTc0Mzk0NzQwLCJpYXQiOjE1NzM3ODk5NDB9.1ShRJ6gscWPNQWRDjt2WzhI92xBmrnpu_sNDowQVbOTytYoF0QWNmDWoQh7FsD7QGbYYji7k8w2hewDgLLG-Bw";

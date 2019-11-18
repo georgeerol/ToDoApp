d025aa55155bbcacaa27d9fa7ac7a9675399a659
George Fouche <fouliex@gmail.com>
2019-11-18 13:39:18 -0800

    Update todo model with lombok

--- src/main/java/com/todo/rest/todo/model/Todo.java
+++ src/main/java/com/todo/rest/todo/model/Todo.java
@@ -1,5 +1,9 @@
 package com.todo.rest.todo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,7 +14,10 @@ import java.util.Date;
  * Created by George Fouche on 6/18/19.
  */
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Data
 public class Todo {
     @Id
     @GeneratedValue
@@ -20,81 +27,6 @@ public class Todo {
     private String description;
     private Date targetDate;
     private boolean isDone;
-
-    public Todo() {
-
-    }
-
-    public Todo(long id, String username, String description, Date targetDate, boolean isDone) {
-        super();
-        this.id = id;
-        this.username = username;
-        this.description = description;
-        this.targetDate = targetDate;
-        this.isDone = isDone;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Date getTargetDate() {
-        return targetDate;
-    }
-
-    public void setTargetDate(Date targetDate) {
-        this.targetDate = targetDate;
-    }
-
-    public boolean isDone() {
-        return isDone;
-    }
-
-    public void setDone(boolean isDone) {
-        this.isDone = isDone;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (int) (id ^ (id >>> 32));
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Todo other = (Todo) obj;
-        if (id != other.id)
-            return false;
-        return true;
-    }
 
 
 }

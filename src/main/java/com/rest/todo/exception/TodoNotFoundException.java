6ad102bc2e5c054b7e929c988861f16496302df2
George Fouche <fouliex@gmail.com>
2019-11-19 11:09:21 -0800

    Add Todo Not Found Exception

---
+++ src/main/java/com/rest/todo/exception/TodoNotFoundException.java
@@ -0,0 +1,11 @@
+package com.rest.todo.exception;
+
+/**
+ * Created by George Fouche on 11/19/19.
+ */
+public class TodoNotFoundException extends RuntimeException {
+
+    public TodoNotFoundException() {
+        super();
+    }
+}
